48e31b5028cc77871a92d2449cfb3c9192c409ef
Erwann <erwann.champion@outlook.fr>
2022-03-18 13:37:24 +0100

    refont

--- front-end/components/Layout.js
+++ front-end/components/Layout.js
@@ -4,7 +4,7 @@ import React from "react";
 
 export default function Layout(props) {
   return (
-    <Flex h={'100vh'} w={'100vw'} bg={"goodfood.gray"}>
+    <Flex h={'100vh'} w={'100vw'} bg={"goodfood.grey"}>
         {props.children}
     </Flex>
 
--- front-end/pages/auth/index.js
+++ front-end/pages/auth/index.js
@@ -1,13 +1,20 @@
-import {Container, Heading} from "@chakra-ui/react";
-import React from "react";
+import { Container, Flex, Heading, Image, LinkOverlay } from '@chakra-ui/react';
+import React, { useState } from 'react';
 
 
-export default function Home() {
+export default function Auth() {
+    const [selected,setSelected]=useState(true);
     return (
+            <div className={"auth"}>
+              <Container bg={"goodfood.white"} className={"container-auth"}>
+                <Image src={"/goodfood-01.svg"}/>
+                <Flex justifyContent={"space-between"} w={"100%"} px={"5rem"}>
+                  <LinkOverlay className={"auth-button"} selected={selected}>Se connecter</LinkOverlay>
+                  <LinkOverlay className={"auth-button"} selected={!selected}>S'inscrire</LinkOverlay>
+                </Flex>
+              </Container>
+            </div>
 
-            <Container h="100%" bg={"goodfood.red"}>
-                <Heading textAlign={"center"} mb={5}>Se connecter</Heading>
-            </Container>
 
     )
 }
