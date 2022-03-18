dc1621342b664fcbc703b52dd6211fa43d8caf6a
Erwann <erwann.champion@outlook.fr>
2022-03-18 08:50:43 +0100

    refon

--- front-end/pages/auth/index.js
+++ front-end/pages/auth/index.js
@@ -1,13 +1,12 @@
 import {Container, Heading} from "@chakra-ui/react";
 import React from "react";
-import FormLogin from "../../components/forms/FormLogin";
+
 
 export default function Home() {
     return (
 
-            <Container h="100%" variant={"white-round"} p={"2.5rem"}>
+            <Container h="100%" bg={"goodfood.red"}>
                 <Heading textAlign={"center"} mb={5}>Se connecter</Heading>
-                <FormLogin />
             </Container>
 
     )
