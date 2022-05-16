62af4f2e2d1766d9826572d468ef1bac44b76de8
Friscxion <erwann.champion@outlook.fr>
2022-05-16 09:31:07 +0200

    Search/locate

--- front-end/pages/home/locate.js
+++ front-end/pages/home/locate.js
@@ -2,6 +2,7 @@ import HomeLayout from '../../components/HomeLayout';
 import { Button, Container, Flex, Heading, Image, Text } from '@chakra-ui/react';
 import GoogleMapReact from 'google-map-react';
 import { MdDinnerDining } from 'react-icons/md';
+import Router from 'next/router';
 
 export default function locate(props){
   let goodfood = {
@@ -26,18 +27,15 @@ export default function locate(props){
       </Flex>
       <Container h={"90vh"} bg={"goodfood.grey"} className={"container-auth"} borderTopRadius={10}>
         <Flex position={"relative"} height={"60vh"} width={"100%"} borderTopRadius={10} overflow={"hidden"}>
-          <Button position={"absolute"} zIndex={"500"}top={5} left={5}>{"< Nos restaurants"}</Button>
+          <Button position={"absolute"} zIndex={"500"}top={5} left={5} onClick={()=>Router.push("/home/search")}>{"< Nos restaurants"}</Button>
           <GoogleMapReact
             bootstrapURLKeys={{ key: "" }}
             defaultCenter={defaultProps.center}
-            defaultZoom={defaultProps.zoom}
-
-          >
-
+            defaultZoom={defaultProps.zoom}>
           </GoogleMapReact>
         </Flex>
-        <Flex>
-          <Flex padding={10} justifyContent={"center"} alignItems={"center"}>
+        <Flex justifyContent={"center"}padding={10}>
+          <Flex marginRight={25} justifyContent={"center"} alignItems={"center"}>
             <MdDinnerDining size={25}/>
           </Flex>
           <Flex flexDirection={"column"} justifyContent={"center"} alignItems={"center"}>
--- front-end/pages/home/search.js
+++ front-end/pages/home/search.js
@@ -1,9 +1,69 @@
 import HomeLayout from '../../components/HomeLayout';
+import {
+  Container,
+  Flex,
+  Heading,
+  Image,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Text,
+} from '@chakra-ui/react';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { useState } from 'react';
+import { GoLocation } from 'react-icons/go';
+import { FaSearchLocation } from 'react-icons/fa';
+import Router from 'next/router';
+
 
 export default function search(props){
+  const [restaurants,setRestaurants]=useState([]);
+
+
+  const renderRestaurants = (restaurant,index) =>{
+    return(
+      <Flex justifyContent={"left"}padding={10} key={index}>
+        <Flex marginRight={25} justifyContent={"center"} alignItems={"center"}>
+          <GoLocation size={25}/>
+        </Flex>
+        <Flex flexDirection={"column"} justifyContent={"center"} alignItems={"center"}>
+          <Heading size={"xs"}>{restaurant.ville+", "+restaurant.pays}</Heading>
+          <Text>{restaurant.adresse}</Text>
+          <Text>{restaurant.tel}</Text>
+        </Flex>
+      </Flex>
+    );
+  }
   return(
     <HomeLayout>
-      <div>search</div>
+      <Flex height={"10vh"} justifyContent={"center"} width={"100%"}>
+        <Image src={"/goodfood-apple.svg"}/>
+      </Flex>
+      <Container h={"90vh"} bg={"goodfood.grey"} className={"container-auth"} borderTopRadius={10}>
+       <Flex padding={10} flexDirection={"column"} maxHeight={"100%"}>
+         <Heading as='h4' size='md'>
+           Nos Restaurants
+         </Heading>
+         <InputGroup marginTop={10}>
+           <Input placeholder='Chercher un restaurant...' />
+           <InputRightElement children={<AiOutlineSearch style={{cursor:"pointer"}}/>} />
+         </InputGroup>
+         <Flex height={"100%"} flexDirection={"column"} overflowY={"scroll"} marginTop={10}>
+           {
+             restaurants.map(renderRestaurants)
+           }
+           <Flex justifyContent={"left"} padding={10} cursor={"pointer"} onClick={()=>Router.push("/home/locate")}>
+             <Flex marginRight={25} justifyContent={"center"} alignItems={"center"}>
+               <FaSearchLocation size={25}/>
+             </Flex>
+             <Flex flexDirection={"column"} justifyContent={"center"} alignItems={"center"}>
+               <Heading size={"xs"}>Ouvrir la carte</Heading>
+             </Flex>
+           </Flex>
+
+         </Flex>
+       </Flex>
+      </Container>
     </HomeLayout>
 
   );
