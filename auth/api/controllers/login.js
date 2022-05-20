1744e3db7c314337e22d0296e222e90b5aa75238
Friscxion <erwann.champion@outlook.fr>
2022-05-20 16:06:19 +0200

    auth register login

---
+++ auth/api/controllers/login.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const login = async (req, res) => {
+  const {usernameEmail,password}=req.body;
+  if(!usernameEmail||!password)
+    return res.sendStatus(406);
+  let user;
+  try{
+    user = await axios.post(process.env.URL_USER+'/email',{email:usernameEmail});
+    if(!user.data) user = await axios.post(process.env.URL_USER+'/username',{username:usernameEmail});
+    if(!user.data) return res.sendStatus(406);
+  }
+  catch(e){
+    return res.sendStatus(403);
+  }
+  let logged = bcrypt.compareSync(password, user.data.password);
+  if(!logged)
+    return res.sendStatus(403);
+
+  let token = jwt.sign({ id: user.data.id }, process.env.SECRET_TOKEN_KEY);
+  res.status(200).json({token:token});
+}
+
+
+module.exports = {login}
--- auth/api/routes/auth.js
+++ auth/api/routes/auth.js
@@ -3,9 +3,10 @@ const router = express.Router();
 
 
 const { register } = require('../controllers/register')
+const { login } = require('../controllers/login');
 
 
 router.post('/register',register);
-
+router.post('/login',login);
 
 module.exports = router;
