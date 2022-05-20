dc127e299f3d99163947dd7433bf7469504b7fe8
Friscxion <erwann.champion@outlook.fr>
2022-05-20 09:48:44 +0200

    refont user start

--- users/api/app.js
+++ users/api/app.js
@@ -1,22 +1,12 @@
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const routes = require('./routes/index');
+const cors = require('cors');
+
 
 const app = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-app.options('/users', function (req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader('Access-Control-Allow-Methods', '*');
-  res.setHeader("Access-Control-Allow-Headers", "*");
-  res.end();
-});
-
-app.use('/users', routes);
 
 module.exports = app;
